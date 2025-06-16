c1c430ba97658cc72413f98418eeb7da8e04502a
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2025-06-16 05:24:20 -0700

    name updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Bilal-Hamid",
+  title: "Moaz-Ahmed",
   description: "Portfolio Website",
 };
 
